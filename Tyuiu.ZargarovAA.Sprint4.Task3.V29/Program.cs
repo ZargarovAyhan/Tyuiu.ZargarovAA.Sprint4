5621ef750984d402da25c94677643f8807a66ffa
Tyuiu.ZargarovAA@outlook.com <fairmarko@gmail.com>
2023-11-05 23:33:29 +0500

    Task3

--- Tyuiu.ZargarovAA.Sprint4.Task3.V29/Program.cs
+++ Tyuiu.ZargarovAA.Sprint4.Task3.V29/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tyuiu.ZargarovAA.Sprint4.Task3.V29.Lib;
 
 namespace Tyuiu.ZargarovAA.Sprint4.Task3.V29
 {
@@ -10,6 +11,66 @@ namespace Tyuiu.ZargarovAA.Sprint4.Task3.V29
     {
         static void Main(string[] args)
         {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнила: Заргаров А.А. | ИИПб-23-2";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы. (статический ввод)                             *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #29                                                             *");
+
+
+            Console.WriteLine("* Выполнила: Заргаров Айхан Ахатович | ИИПб-23-2                          *");
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов,                    *");
+            Console.WriteLine("* заполненный статическими значениями в диапазоне от 5 до 9.              *");
+            Console.WriteLine("* Найдите произведение элементов в первом столбце массива.                *");
+
+
+            Console.WriteLine("* 9, 9, 8, 6, 9,                                                          *");
+            Console.WriteLine("* 5, 8, 8, 8, 7,                                                          *");
+            Console.WriteLine("* 6, 5, 9, 7, 9,                                                          *");
+            Console.WriteLine("* 7, 7, 9, 7, 8,                                                          *");
+
+            Console.WriteLine("* 8, 5, 8, 5, 5,                                                          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] mtrx = new int[5, 5] { { 9, 9, 8, 6, 9 },
+                                          { 5, 8, 8, 8, 7 },
+                                          { 6, 5, 9, 7, 9 },
+                                          { 7, 7, 9, 7, 8 },
+                                          { 8, 5, 8, 5, 5 } };
+
+            Console.WriteLine("Массив:");
+
+            for (int i = 0; i < mtrx.GetLength(0); i++)
+            {
+                for (int j = 0; j < mtrx.GetLength(1); j++)
+                {
+                    Console.Write(mtrx[i, j].ToString().PadLeft(3) + " ");
+
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+
+            Console.WriteLine("***************************************************************************");
+
+            int res = ds.Calculate(mtrx);
+
+            Console.WriteLine("Произведение элементов в первом столбце: " + res);
+            Console.ReadKey();
         }
     }
 }
